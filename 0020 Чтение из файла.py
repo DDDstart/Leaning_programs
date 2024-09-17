c70873067b03b63f686f81b3d13d602e75cf20b1
ddd <androzh@yandex.ru>
2024-09-18 00:51:25 +0300

    update

--- 0020 Чтение из файла.py
+++ 0020 Чтение из файла.py
@@ -49,7 +49,7 @@ a3b4c2e10b1
 Sample Output:
 aaabbbbcceeeeeeeeeeb
 '''
-# Программа с использование ChatGPT
+# Программа с использованием ChatGPT
 def decode_string(encoded_string):
     decoded_string = []
     i = 0
@@ -68,3 +68,169 @@ decoded_string = decode_string(encoded_string) # Декодирование ст
 with open('output.txt', 'w') as output_file: # Запись результата в файл
     output_file.write(decoded_string)
 print("Decoded string has been written to 'output.txt'.")
+
+
+'''Недавно мы считали для каждого слова количество его вхождений в строку. Но на все слова может быть не так интересно смотреть,
+как, например, на наиболее часто используемые. Напишите программу, которая считывает текст из файла (в файле может быть больше
+одной строки) и выводит самое частое слово в этом тексте и через пробел то, сколько раз оно встретилось. Если таких слов
+несколько, вывести лексикографически первое (можно использовать оператор < для строк). В качестве ответа укажите вывод программы,
+а не саму программу. Слова, написанные в разных регистрах, считаются одинаковыми.
+
+Sample Input:
+abc a bCd bC AbC BC BCD bcd ABC
+
+Sample Output:
+abc 3
+'''
+# Программа с использованием Copilot:
+from collections import Counter
+def most_common_word(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        text = file.read().lower().split()
+    word_counts = Counter(text)
+    most_common = word_counts.most_common()
+    # Найти лексикографически первое слово среди самых частых
+    max_count = most_common[0][1]
+    most_common_words = [word for word, count in most_common if count == max_count]
+    result_word = min(most_common_words)
+    return result_word, word_counts[result_word]
+# Пример использования
+filename = 'input.txt'
+word, count = most_common_word(filename)
+print(word, count)
+
+#
+# Другое решение:
+s, d, m, w = str(), dict(), 0, str()
+with open("dataset_3363_3.txt", "r") as f:
+    s = f.read().lower().strip().split()
+s.sort()
+for word in s:
+    if word in d:
+        d[word] += 1
+    else:
+        d[word] = 1
+for word in d:
+    if d[word] > m:
+        m = d[word]
+        w = word      
+
+#
+# Другое решение:
+all = open('dataset.txt', 'r')
+s = all.read().lower().strip().split()
+y = 0
+m =''
+for i in s:
+    z = s.count(i)
+    if z >= y:
+        y = z
+        m = i
+print(m, y)
+
+
+'''Имеется файл с данными по успеваемости абитуриентов. Он представляет из себя набор строк, где в каждой строке записана следующая информация:
+Фамилия;Оценка_по_математике;Оценка_по_физике;Оценка_по_русскому_языку
+Поля внутри строки разделены точкой с запятой, оценки — целые числа.
+Напишите программу, которая считывает исходный файл с подобной структурой и для каждого абитуриента записывает его среднюю оценку по трём предметам на отдельной строке, соответствующей этому абитуриенту, в файл с ответом.
+Также вычислите средние баллы по математике, физике и русскому языку по всем абитуриентам и добавьте полученные значения, разделённые пробелом, последней строкой в файл с ответом.
+В качестве ответа на задание прикрепите полученный файл со средними оценками по каждому ученику и одной строкой со средними оценками по трём предметам.
+Примечание. Для разбиения строки на части по символу ';' можно использовать метод split следующим образом:
+
+print('First;Second-1 Second-2;Third'.split(';'))
+# ['First', 'Second-1 Second-2', 'Third']
+
+Sample Input:
+Петров;85;92;78
+Сидоров;100;88;94
+Иванов;58;72;85
+
+Sample Output:
+85.0
+94.0
+71.666666667
+81.0 84.0 85.666666667
+'''
+# Программа с использованием Copilot:
+def calculate_averages(input_filename, output_filename):
+    with open(input_filename, 'r', encoding='utf-8') as infile:
+        lines = infile.readlines()
+    total_math, total_physics, total_russian = 0, 0, 0
+    num_students = len(lines)
+    results = []
+    for line in lines:
+        parts = line.strip().split(';')
+        surname = parts[0]
+        math = int(parts[1])
+        physics = int(parts[2])
+        russian = int(parts[3])
+        average = (math + physics + russian) / 3
+        results.append(f"{average:.10f}")
+        total_math += math
+        total_physics += physics
+        total_russian += russian
+    avg_math = total_math / num_students
+    avg_physics = total_physics / num_students
+    avg_russian = total_russian / num_students
+    with open(output_filename, 'w', encoding='utf-8') as outfile:
+        for result in results:
+            outfile.write(result + '\n')
+        outfile.write(f"{avg_math:.10f} {avg_physics:.10f} {avg_russian:.10f}\n")
+
+# Пример использования
+input_filename = 'input.txt'
+output_filename = 'output.txt'
+calculate_averages(input_filename, output_filename)
+
+# Пояснения к этой задаче
+'''
+# Для тех, кто хочет сократить свой код :) написал небольшое руководство по [list comprehension]
+# на основе примера на stackoverflow.com
+# # http://stackoverflow.com/questions/16632124/python-emulate-sum-using-list-comprehension
+# я немного изменил этот пример, чтобы лучше объяснить работу [list comprehension]
+# и вам было проще понять, как применить этот подход к решению задания
+
+# допустим, у нас есть список фруктов, где зафиксированы самые низкие и высокие цены на эти фрукты
+# т.е. по сути это список списков :)
+lst = [["apple", 55, 62], ["orange", 60, 74], ["pineapple", 140, 180], ["lemon", 80, 84]]
+
+# выведем этот список для нагляности на экран, используя [list comprehension]
+[print(el) for el in lst]
+# ['apple', 55, 62]
+# ['orange', 60, 74]
+# ['pineapple', 140, 180]
+# ['lemon', 80, 84]
+
+# если мы хотим подсчитать среднюю цену на каждый из фруктов, то напишем что-то вроде
+sumMiddle = 0
+for el in lst:
+    sumMiddle = (el[1] + el[2]) / 2
+    print(sumMiddle)
+
+# или можно сделать это одной строкой
+[print((priceLow + priceHigh) / 2) for fruit, priceLow, priceHigh in lst]
+# представьте, что наш список списков - это таблица из трёх столбцов
+# и мы можем обращаться к столбцам, просто озаглавив их fruit, priceLow, priceHigh
+# в цикле for, почти как перебор элементов словаря for key, value in d.items() :)
+
+# поэтому, когда вы захотите прикинуть, сколько же, от и до, в среднем может стоить
+# ваша фруктовая корзина, нужно будет посчитать среднее по каждой колонке
+# вы можете сделать это примерно так
+sumLow, sumHigh = 0, 0
+for el in lst:
+    sumLow += el[1]
+    sumHigh += el[2]
+sumLow /= len(lst)
+sumHigh /= len(lst)
+print(sumLow, sumHigh)
+
+# или применить кунг-фу списковых выражений и обойтись парой строк :)
+print(sum([priceLow for fruit, priceLow, priceHigh in lst]) / len(lst))
+print(sum([priceHigh for fruit, priceLow, priceHigh in lst]) / len(lst))
+
+# а где два принта, там и один :)
+print(sum([priceLow for fruit, priceLow, priceHigh in lst]) / len(lst), sum([priceHigh for fruit, priceLow, priceHigh in lst]) / len(lst))
+
+# надеюсь, вам было понятно и интересно
+# желаю успехов в учёбе!!!
+'''
